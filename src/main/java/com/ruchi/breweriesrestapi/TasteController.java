c6bbe2f801f436858b187e93ad120fea6fd48e85
Ruchi Devi <ruchidevi1987@gmail.com>
2020-04-29 17:50:03 +0100

    QR code

--- src/main/java/com/ruchi/breweriesrestapi/TasteController.java
+++ src/main/java/com/ruchi/breweriesrestapi/TasteController.java
@@ -5,17 +5,25 @@ import com.google.zxing.client.j2se.MatrixToImageWriter;
 import com.google.zxing.common.BitMatrix;
 import com.google.zxing.oned.EAN13Writer;
 import java.awt.image.BufferedImage;
+import java.io.OutputStream;
+import javax.servlet.http.HttpServletResponse;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/Taste")
 public class TasteController {
-
+    
+    @GetMapping(value = "/code/{BreweryId}", produces = MediaType.IMAGE_PNG_VALUE)
+    public ResponseEntity<BufferedImage> okResponse(BufferedImage image) {
+        return new ResponseEntity<>(image, HttpStatus.OK);
+    }
     @GetMapping(value = "/code/{BreweryId}", produces = MediaType.IMAGE_PNG_VALUE)
     public ResponseEntity<BufferedImage> barbecueEAN13Barcode(@PathVariable("barcode") String barcode) throws Exception {
         EAN13Writer barcodeWriter = new EAN13Writer();
@@ -23,7 +31,16 @@ public class TasteController {
 
         //example from https://www.baeldung.com/java-generating-barcodes-qr-codes
         //FIXME example does not say what static import was used to get okResponse() method.
-        //return okResponse(MatrixToImageWriter.toBufferedImage(bitMatrix));
-        return null;
+          return okResponse(MatrixToImageWriter.toBufferedImage(bitMatrix));
+        
     }
+    
+    	@RequestMapping(value = "code/{BreweryId}", method = RequestMethod.GET)
+	public void qrcode(@PathVariable("BreweryId") int BreweryId, HttpServletResponse response) throws Exception {
+		response.setContentType("image/png");
+		OutputStream outputStream = response.getOutputStream();
+		outputStream.write(ZXingHelper.getQRCodeImage(id, 200, 200));
+		outputStream.flush();
+		outputStream.close();
+	}
 }
---
+++ src/main/java/com/ruchi/breweriesrestapi/ZXingHelper.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.ruchi.breweriesrestapi;
+
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.client.j2se.MatrixToImageWriter;
+import com.google.zxing.common.BitMatrix;
+import com.google.zxing.qrcode.QRCodeWriter;
+import java.io.ByteArrayOutputStream;
+
+/**
+ *
+ * @author Ruchi Devi
+ */
+class ZXingHelper {
+    public static byte[] getQRCodeImage(String text, int width, int height) {
+		try {
+			QRCodeWriter qrCodeWriter = new QRCodeWriter();
+			BitMatrix bitMatrix = qrCodeWriter.encode(text, BarcodeFormat.QR_CODE, width, height);
+			ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+			MatrixToImageWriter.writeToStream(bitMatrix, "png", byteArrayOutputStream);
+			return byteArrayOutputStream.toByteArray();
+		} catch (Exception e) {
+			return null;
+		}
+	}
+    
+}
